9f20cd6406fe36cda1f0b09a18c1168becd1847f
rahmatafriyanto24 <afriyantor4942@gmail.com>
2024-12-07 09:50:34 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,8 @@ const InputError = require("../exceptions/InputError");
 
 (async () => {
   const server = Hapi.server({
-    port: process.env.APP_PORT || 3000,
-    host: process.env.APP_HOST || "localhost",
+    port: process.env.APP_PORT || 8080,
+    host: process.env.APP_HOST || "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
